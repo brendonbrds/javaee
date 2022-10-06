1a67525c77346a2dfee454157ff5e34d80fa91f3
secinfo <secinfo@debian-BULLSEYE-live-builder-AMD64>
2022-10-06 12:48:11 -0300

    firstttttt

--- src/launcher/ExecuteAutoLogs.java
+++ src/launcher/ExecuteAutoLogs.java
@@ -1,7 +1,5 @@
 package launcher;
 
-import java.awt.Dimension;
-import java.awt.Font;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,8 +7,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-import javax.swing.plaf.FontUIResource;
 
 public class ExecuteAutoLogs {
 
@@ -21,15 +17,15 @@ public class ExecuteAutoLogs {
 			bw.write("Inicio em " + getDate());
 
 			// INICIO DE CONFIGURACOES DO PC
-			UIManager.put("OptionPane.messageFont", new FontUIResource(new Font("Courier New", Font.BOLD, 14)));
-			UIManager.put("OptionPane.minimumSize", new Dimension(600, 400));
+//			UIManager.put("OptionPane.messageFont", new FontUIResource(new Font("Courier New", Font.BOLD, 14)));
+//			UIManager.put("OptionPane.minimumSize", new Dimension(600, 400));
 			JOptionPane.showMessageDialog(null, getMessage(), "INFORMATIVO", JOptionPane.INFORMATION_MESSAGE);
 			// FIM DE CONFIGURACOES DO PC
 
 			bw.write(" | Fim em " + getDate());
 		} catch (Exception e) {
-			UIManager.put("OptionPane.minimumSize", new Dimension(600, 400));
-			UIManager.put("OptionPane.messageFont", new FontUIResource(new Font("Courier New", Font.BOLD, 14)));
+//			UIManager.put("OptionPane.minimumSize", new Dimension(600, 400));
+//			UIManager.put("OptionPane.messageFont", new FontUIResource(new Font("Courier New", Font.BOLD, 14)));
 			JOptionPane.showMessageDialog(null, e.getMessage(), "Algo de errado não está certo!", JOptionPane.ERROR_MESSAGE);
 		} finally {
 			if (bw != null)
@@ -46,12 +42,12 @@ public class ExecuteAutoLogs {
 	}
 
 	public static String getMessage() {
-		String message = "SecInfo 2BECmb - Normas gerais - Evitar uso de pendrives \n - Nao compartilhar suas senhas \n "
+		String message = "SecInfo 2BECmb - Normas gerais \n\n - Evitar uso de pendrives \n - Nao compartilhar suas senhas \n "
 				+ "- Alterar as senhas padrões e usar senhas fortes \n - Utilizar softwares do catálogo da Seção de Informática \n "
 				+ "- Não faça uso de NENHUMA outra rede, além da EBNet \n - Não modificar configurações administrativas do computador \n "
 				+ "- Comunicar qualquer incidente de segurança imediatamente a Seção de Informática \n"
 				+ "- Os PCs verificados pela Seção de Informática possuem etiqueta padrão, lacres, \n  papel de parede com a logo da OM e bloqueios diversos que impedem mudanças no sistema. \n"
-				+ "- Todos computadores devem obecer os padrões estabelecidos pela Seção de Informática. \n Em caso de dúvida, procure a Seção de Informática para regularizar seu equipamento.";
+				+ "- Todos computadores devem obecer os padrões estabelecidos pela Seção de Informática. \n\n  Em caso de dúvida, procure a Seção de Informática para regularizar seu equipamento.";
 
 		return message;
 	}
